29d9e35cb56d7790c9f3cfef63d3055a1115deb5
Garrett Ellis <154717520+garrettbovo@users.noreply.github.com>
2024-01-21 19:58:02 -0500

    Add files via upload

---
+++ Functions/implementAction.py
@@ -0,0 +1,43 @@
+from Functions import displayRestaurants, displayRestRatings, validIndex, amendRatings, displayIndexRating
+
+def implementAction(restaurants, action):
+    """This function determines who the flow of the program will run based on the user's menu selection.  This function takes in two arguments into its parameter list: restaurants and action."""
+
+    #declaration and initialization of variables
+    index = 0
+    ratingIndex = 0
+
+    #conditional statement which checks if user's input is either "a" or "A"
+    if action == "a" or action == "A":
+        #calling displayRestaurants function to display all the restaurants in the system
+        displayRestaurants.displayRestaurants(restaurants)
+
+    #conditional statement which checks if user's input is either "b" or "B"    
+    elif action == "b" or action == "B":
+        print("\nDisplay all ratings for a restaurant...")
+        #getting a valid restaurant index from user by calling validIndex function
+        index = validIndex.validIndex(restaurants, "length of restaurant")   
+        #calling displayRestaurants function to display all the restaurant's in the system          
+        displayRestRatings.displayRestRatings(restaurants[index])  
+
+    #conditional statement which checks if user's input is either "c" or "C"
+    elif action == "c" or action == "C":
+        print("\nAdd a rating to a restaurant...")
+        #getting a valid restaurant index from user by calling validIndex function
+        index = validIndex.validIndex(restaurants, "length of restaurant")
+        #calling addRating function to add a rating to the specified restaurant
+        amendRatings.amendRatings(restaurants[index])
+
+    #conditional statement which checks if user's input is either "d" or "D"
+    elif action == "d" or action == "D" or action == "e" or action == "E":
+        print()
+        #getting a valid restaurant index from user by calling validIndex function
+        index = validIndex.validIndex(restaurants, "length of restaurant")
+        #getting a valid rating index from the user by calling validIndex function 
+        ratingIndex = validIndex.validIndex(restaurants[index], "number of ratings") 
+        #calling displayIndexRating function to display a rating at the specified restaurant and at the specified index
+        displayIndexRating.displayIndexRating(restaurants[index], ratingIndex)
+        if action == "e" or action == "E":
+            #calling addRating function to add a rating to the specified restaurant
+            amendRatings.amendRatings(restaurants[index], ratingIndex)     
+        
---
+++ README.md
@@ -0,0 +1,33 @@
+![video_poster_1](https://github.com/Python-Coding-Assignments/Restaurant-Ratings-Part-1/assets/154717520/f187d60f-69a1-456e-a825-24bcd4ab554b)
+
+<details>
+<summary>Table of Contents</summary>
+<ol>
+  <li>
+    <a href='#about-the-project'>About the Project</a>
+  </li>
+  <li>
+    <a href='#getting-started'>Getting Started</a>
+  </li>
+  <li>
+    <a href='#Contact'>Contact</a>
+  </li>  
+</ol>
+</details>
+
+## About the Project
+This project simulates a simple application where information about a restaurant is made available to the user.  The user can do any of the following: print all restaurants (including information about each restaurant), print all ratings for a restaurant specified by the user, add more ratings to a restaurant he or she specifies, print a specified restaurant's rating, and lastly edit a specified restaurant's rating.
+
+The objective of this program is to demonstrate the following: 
+* show a basic understanding of classes and how they work, including the init constructor, setters, and getters
+* utilize packages to organize classes and functions in different directories
+* introduce the concept of encapsulation into a simple program and work with attributes that are private to the user
+
+In this program, the user can enter anything, even when prompted with entering an integer menu selection, and the program will still function as intended. 
+
+## Getting Started
+To run the program, download the contents of the project and simply run make in the terminal.
+
+## Contact
+Name: Garrett Ellis\
+Project Link: [https://github.com/Python-Coding-Assignments/Restaurant-Ratings-Part-3]()
